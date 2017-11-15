4bf044c4f9d6be71c9e7fa756482f980db437ee7
Tony <ANCLIST@GMAIL.COM>
2017-11-15 15:07:18 -0500

    Ajax Json request and response

--- election.js
+++ election.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var body = document.querySelector('body');
+  var list = document.createElement('ul')
+  body.append(list)
 
-  // Imagination!
+$.ajax({
+  url: 'https://bb-election-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'json',
+}).done(function(data) {
+  for (var i = 0; i < data.candidates.length; i++) {
+    var candidate = document.createElement('li');
+    var result = document.createElement('p');
+
+    result.innerText = `Name: ${data.candidates[i].name}, Total Votes: ${data.candidates[i].votes}`;
+
+    candidate.append(result);
+    list.append(candidate);
+  };
+});
 
 });
